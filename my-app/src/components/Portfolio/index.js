fe03d22c3acff9d350a2f8111e82858505be3860
matthewjflanagan <mattflanagan40@gmail.com>
2022-01-31 20:26:59 -0500

    data and input added to each component

--- my-app/src/components/Portfolio/index.js
+++ my-app/src/components/Portfolio/index.js
@@ -8,15 +8,36 @@ const Portfolio = () => {
       name : 'HR Management System',
       img : './HRManagementSystem.png',
       repo : 'https://github.com/alifaizan786-op/HR_Management_System',
-      description : 'kdjfalksjflksdj'
+      description : 'HTML/CSS/Materialize/Javascript/Node.js/SQL/Handlebars'
     },
     {
       id : 2,
       name : 'JOBS 4 MORE',
       img : './openingpagewithoutdropdown.png',
       repo : 'https://matthewjflanagan.github.io/JOBS-4-MORE/',
-      description : 'blah'
-    }
+      description : 'HTML/CSS/Javascript'
+    },
+    {
+      id : 3,
+      name : 'Tech Blog',
+      img : './techBlog.png',
+      repo : 'https://github.com/matthewjflanagan/Tech-Blog',
+      description : 'HTML/CSS/Materialize/Javascript/Node.js/SQL/Handlebars'
+    },
+    {
+      id : 4,
+      name : 'Code Quiz',
+      img : './runningapp.png',
+      repo : 'https://github.com/matthewjflanagan/Code-Quiz',
+      description : 'HTML/CSS/Javascript'
+    },
+    {
+      id : 5,
+      name : 'Squid Games',
+      img : './squid-games.PNG',
+      repo : 'https://github.com/matthewjflanagan/Squid-Games',
+      description : 'HTML/CSS/Javascript'
+    },
   ];
 
   return (
