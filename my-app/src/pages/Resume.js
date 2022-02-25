dec0d4444010fd6dd31933c72ada66169debcf4c
matthewjflanagan <mattflanagan40@gmail.com>
2022-02-25 08:42:49 -0500

    resume styling

--- my-app/src/pages/Resume.js
+++ my-app/src/pages/Resume.js
@@ -4,11 +4,11 @@ import React from "react";
 
 function Resume() {
   return (
-    <div className="contact-card">
+    <div className="contactCardDiv">
 
-      <div className="card">
-        <h1 className="text-center">Resume</h1>
-        <div className="card-content">
+      <div className="contactCard">
+        <h1>Resume</h1>
+        <div className="cardContent">
           <a href="https://docs.google.com/document/d/195risknuNCpM10VRRABUB_qUqYfKepkBHhV7aiVFkPY/edit?usp=sharing"><h1 className="text-center mt-2">Download  Here</h1></a>
         </div>
       </div>
